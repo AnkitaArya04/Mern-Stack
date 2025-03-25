4d97945cd034908b2fbd21998f0fbd4fca34da31
Ankita Arya <ankitaarya1207@gmail.com>
2025-03-25 22:43:32 +0530

    JS Intro

---
+++ video 41/script.js
@@ -0,0 +1,15 @@
+alert("Hello world")
+console.log("Ankita")
+console.log("Ankita")
+console.log("Ankita")
+console.log("Ankita")
+
+var a=prompt("Enter your number")
+// console.log("your number is "+ a)
+var isTrue=confirm("Are you sure you want to leave this page and blast ypur computer")
+if(isTrue){
+    console.log("computer is blasting")
+}else{
+    console.log("computer is not blasting")
+}
+console.log("your number is "+ a)
