bd40066cf84186d0780ac47adbfc0d72336a422b
Ankita Arya <ankitaarya1207@gmail.com>
2025-03-26 17:28:13 +0530

    JS variables

---
+++ video 42/script.js
@@ -0,0 +1,32 @@
+console.log("Hey this is javascript")
+// var a=9;
+// var b=9;
+// var c="ankita"
+let a=3;
+let b=5;
+let c="ankita";
+console.log(a+b);
+console.log(typeof a,typeof b,typeof c)
+{
+    let a=77
+    console.log(a)
+}
+console.log(a)
+// const a1=8;
+// a1=a1+4  //not allowed a1 is constant
+let x="ankita arya";
+let y=22;
+let z=2.33;
+const p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,x,p,q,r)
+console.log(typeof x,typeof y,typeof x,typeof p,typeof q,typeof r)
+let o={
+    "name":"Ankita",
+    "job code":839
+}
+console.log(o);
+o.salary="1000"
+console.log(o)
+
