183efbaec5e9a9361744e639169a2a6076030915
Ankita Arya <ankitaarya1207@gmail.com>
2025-03-26 22:25:40 +0530

    String in JS

---
+++ video 46/index.js
@@ -0,0 +1,18 @@
+console.log("welcome String")
+let a="Ankita"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a.length)
+
+let name ="Ankita"
+let friend="Vaibhavi"
+// console.log("Her name is "+ name +" and her friend name is "+ friend)
+// console.log(`her name is ${name}`)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(1))
+console.log(name.replace("An","ha"))
+console.log(name.concat(friend,"fkjf","kjckfa"))
+// console.log(name.concat)
