04f544f16bffbc1152460aa05966d0c8c72eee39
Ankita Arya <ankitaarya1207@gmail.com>
2025-03-28 13:41:03 +0530

    Arrays

---
+++ 'video 48/index.js
@@ -0,0 +1,9 @@
+let arr=[2,34,5,5,7];
+arr[0]=354
+// console.log(arr, typeof arr)
+// console.log(arr.length)
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[0])
+console.log(arr.toString())
+console.log(arr.join(" and "))
---
+++ 'video 48/loops.js
@@ -0,0 +1,25 @@
+let arr=[1,93,5,6,88]
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i])
+// }
+
+// a.forEach((value,index,arr)=>{
+//     console.log(value,index,arr)
+// })
+// let obj={
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// for (const key in obj) {
+//     if(Object.hasOwnProperty.call(obj,key)){
+//         const element=obj[key];
+//         console.log(key,element)
+//     }
+    
+// }
+
+for (const iteraor of arr) {
+    console.log(iteraor)
+    
+}
