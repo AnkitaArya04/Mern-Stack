e7e9b54b1a22368f413476c02c01b09ba33db023
Ankita Arya <ankitaarya1207@gmail.com>
2025-04-03 15:27:27 +0530

    CommonJs and EcmaScript

---
+++ video 66/main.js
@@ -0,0 +1,14 @@
+// import {a,b,d} from "./mymodule.js"
+// console.log(a,b,d)
+
+// import ankita from "./mymodule.js"
+// console.log(ankita)
+
+
+// (function(exports,require,module,__dirname,__filename){
+//     //module code actually lives here
+// })
+
+
+const a=require("./mymodule2.js")
+console.log(a, __dirname, __filename)
